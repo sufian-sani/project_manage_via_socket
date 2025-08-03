7195299e9180822f693126fbfeda2dce1783e4bd
abu-sani <abu.sani@byslglobal.com>
2025-08-03 16:14:53 +0600

    few modified

--- tracker/utils.py
+++ tracker/utils.py
@@ -10,6 +10,6 @@ def send_bug_assignment_email(bug):
             subject="Bug Assigned",
             message="You have been assigned a new bug.",
             from_email=settings.DEFAULT_FROM_EMAIL,                # or use settings.DEFAULT_FROM_EMAIL
-            recipient_list=["abu5suffian@gmail.com",],     # real recipient
+            recipient_list=["abu6suffian@gmail.com",],     # real recipient
             fail_silently=False,
         )
